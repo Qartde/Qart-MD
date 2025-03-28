6131ae51a08edaaa8a1787aed31820aad0a58bbd
Qartde <bonifacemd2525@gmail.com>
2025-03-28 13:24:03 +0300

    Update GroupQuotes.js

--- commandes/GroupQuotes.js
+++ commandes/GroupQuotes.js
@@ -13,13 +13,13 @@ zokou({ nomCom: 'quote', categorie: 'Group' }, async (dest, zk, commandeOptions)
       fetch('https://animechan.xyz/api/random')
         .then((response) => response.json())
         .then(async (quote) => {
-          repondre(`*YESSER-MDQuotes*
+          repondre(`*QART-MDQuotes*
 
 🎬 Anime: ${quote.anime}
 👤 Character: ${quote.character}
 💬 Quote: ${quote.quote}
 
-Powered by *yessertech*`);
+Powered by *QartMd*`);
         });
     } catch (e) {
       repondre('Erreur lors de la génération de la citation : ' + e.message);
@@ -31,13 +31,13 @@ Powered by *yessertech*`);
       fetch('https://animechan.xyz/api/random/character?name=' + query)
         .then((response) => response.json())
         .then(async (quote) => {
-          repondre(`YESSER-MD
+          repondre(`QART-MD
 
 🎬 Anime: ${quote.anime}
 👤 Character: ${quote.character}
 💬 Quote: ${quote.quote}
 
-Powered by *yessertech*`);
+Powered by *QartMd*`);
         });
     } catch (e) {
       repondre('Erreur lors de la génération de la citation : ' + e.message);
