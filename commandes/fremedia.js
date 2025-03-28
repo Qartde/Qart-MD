f3ff50b0c625143ae19134bb1652ccc6518d089c
Qartde <bonifacemd2525@gmail.com>
2025-03-28 15:27:22 +0300

    Update fremedia.js

--- commandes/fremedia.js
+++ commandes/fremedia.js
@@ -24,7 +24,7 @@ zokou({
       'image': {
         'url': _0x93b128
       },
-      'caption': "*powered by YESSER_MD*"
+      'caption': "*powered by QART_MD*"
     }, {
       'quoted': _0x4eb9fe
     });
